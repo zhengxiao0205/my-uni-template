5afc4dfbfc348bb421a7999f907886463675dcac
zhengxiao <zhengxiao0205@outlook.com>
2024-06-04 12:06:04 +0800

    搭建uniapp模板

---
+++ src/pages/home/home.vue
@@ -0,0 +1,25 @@
+<template>
+    <view>
+      <u-navbar title="首页" @rightClick="rightClick" @leftClick="leftClick" :autoBack="false"> </u-navbar>
+    </view>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {}
+    },
+    onLoad() {},
+    methods: {
+      rightClick() {
+        console.log('rightClick')
+      },
+      leftClick() {
+        console.log('leftClick')
+      },
+    },
+  }
+  </script>
+  
+  <style></style>
+  
---
+++ src/pages/my/my.vue
@@ -0,0 +1,31 @@
+<template>
+  <view>
+    <u-navbar title="我的" @rightClick="rightClick" @leftClick="leftClick" :autoBack="false"> </u-navbar>
+  </view>
+</template>
+
+<script>
+import { getList, updateData } from '@/api/demo'
+export default {
+  data() {
+    return {}
+  },
+  onLoad() {
+    getList().then((res) => {
+      console.log(res)
+    })
+  },
+  methods: {
+    rightClick() {
+      console.log('right click')
+    },
+    leftClick() {
+      updateData({ name: 'new name' }).then((res) => {
+        console.log(res)
+      })
+    },
+  },
+}
+</script>
+
+<style></style>
